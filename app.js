b4b45c10c637e16e898cc8e82a524cecae7af775
Juan La Jara <juanlajara@gmail.com>
2015-04-28 21:50:05 -0500

    add pouchdb and load some messages

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
+var db = new PouchDB( 'http://limajs.iriscouch.com/limajs-chat' );
+
 // Empezamos jQuery
 $( function () {
-	console.log( 'DOM ha cargado' );
+
+	var $messageHolder = $('div.js-messages-holder')
+	  , parseMessages
+	  ;
+
+	parseMessages = function ( messages ) {
+
+		$.each( messages, function ( index, value ) {
+
+			$('<div>')
+			  .addClass('message')
+			  .text( value.doc.message.content )
+			  .appendTo( $messageHolder );
+		} );
+	};
+
+	db.allDocs( { include_docs: true }, function ( err, result ) {
+
+		parseMessages( result.rows );
+	} );
 } );
