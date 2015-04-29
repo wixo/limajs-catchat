0f49cfd4ec0604a49a590901233b49d0989a2c76
Juan La Jara <juanlajara@gmail.com>
2015-04-29 00:53:36 -0500

    add autorefresh + cats

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ $( function () {
 
 	var $messagesHolder = $('div.js-messages-holder')
 	// fn declarations
-	  , parseMessages
 	  , buildMessageHolder
 	  , buildMessageAuthor
 	  , buildMessageContent
 	  , buildMessageDate
+	  , parseMessages
+	  , putDataInDB
 	  , sendMessage
+	  , showMessages
 	  ;
 
 	moment.locale('es-ES');
@@ -19,7 +21,7 @@ $( function () {
 		var $messageHolder;
 
 		$messageHolder = $('<div>')
-		  .addClass( 'message-holder' )
+		  .addClass( 'message-holder js-message-holder' )
 		  .appendTo( $messagesHolder );
 
 		buildMessageAuthor( value ).appendTo( $messageHolder );
@@ -36,9 +38,16 @@ $( function () {
 
 	buildMessageContent = function ( value ) {
 
-		return $('<div>')
-		  .addClass( 'message-part message-content' )
-		  .text( value.doc.message.content );
+		if ( value.doc.isACat ) {
+			return $('<div>')
+			  .addClass( 'message-part message-content' )
+			  .append( $('<img>').prop('src', value.doc.message.content ) );
+		} else {
+			return $('<div>')
+			  .addClass( 'message-part message-content' )
+			  .text( value.doc.message.content );
+		}
+
 	};
 
 	buildMessageDate = function ( value ) {
@@ -49,15 +58,38 @@ $( function () {
 	};
 
 	parseMessages = function ( messages ) {
-
+		$messagesHolder.empty();
 		messages.forEach( buildMessageHolder );
+		$messagesHolder.scrollTop( $('div.js-messages-holder')[0].scrollHeight );
+		$('input.js-cat-checkbox').prop('checked') && $('input.js-cat-checkbox').click();
+		$messagesHolder.find( 'div.js-message' ).focus();
 	};
 
-	sendMessage = function () {
+	putDataInDB = function ( messageData, $message ) {
+		db.put( messageData )
+		.then( function ( result ) {
+
+			$('p.js-status-message').text( 'Mensaje enviado!' );
+			$message.val('');
+		} )
+		.catch( function ( error ) {
+
+			console.log( 'Hubo un error', error, error.stack );
+		} );
+	};
+
+	sendMessage = function ( e ) {
 		var $nickname   = $('input.js-nickname')
 		  , $message    = $('input.js-message')
+		  , isACat      = $('input.js-cat-checkbox').prop('checked')
 		  , messageData = {}
 		  ;
+
+		e.preventDefault();
+
+		if ( isACat ) {
+			$message.val( 'http://lorempixel.com/200/200/cats/' + Math.floor( Math.random() * 10 ) );
+		}
 
 		if ( $nickname.val() !== '' && $message.val() !== '' ) {
 
@@ -65,15 +97,12 @@ $( function () {
 			messageData.author  = $nickname.val();
 			messageData.message = { content: $message.val(), date: new Date() };
 
-			db.put( messageData )
-			.then( function ( result ) {
+			if ( isACat ) {
+				messageData.isACat = true;
+			}
 
-				$('p.js-status-message').text( 'Mensaje enviado!' );
-			} )
-			.catch( function ( error ) {
+			putDataInDB( messageData, $message );
 
-				console.log( 'Hubo un error', error, error.stack );
-			} );
 		} else {
 
 			$('p.js-status-message').text( 'Hay campos vacios!' );
@@ -81,15 +110,27 @@ $( function () {
 
 	};
 
-	$('button.js-submit-btn').on('click', sendMessage );
+	showMessages = function () {
 
-	db.allDocs( { include_docs: true } )
-	.then( function ( result ) {
+		db.allDocs( { include_docs: true } )
+		.then( function ( result ) {
 
-		parseMessages( result.rows );
-	} )
-	.catch( function ( error ) {
+			parseMessages( result.rows );
+		} )
+		.catch( function ( error ) {
 
-		console.log( 'Hubo un error', error, error.stack );
-	} );
+			console.log( 'Hubo un error', error, error.stack );
+		} );
+	};
+
+	db.changes( { since : 'now'
+	            , live  : true
+	            } )
+	.on('change', showMessages);
+
+	showMessages();
+
+	$('form.js-new-message-form').on('submit', sendMessage );
+
+
 } );
