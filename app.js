e79b78e38cf81a907033d262b5f03a4ce55fa5e8
Juan La Jara <juanlajara@gmail.com>
2015-04-28 22:29:14 -0500

    add nick and message form and add better parsing of messages

--- app.js
+++ app.js
@@ -3,23 +3,62 @@ var db = new PouchDB( 'http://limajs.iriscouch.com/limajs-chat' );
 // Empezamos jQuery
 $( function () {
 
-	var $messageHolder = $('div.js-messages-holder')
+	var $messagesHolder = $('div.js-messages-holder')
+	// fn declarations
 	  , parseMessages
+	  , buildMessageHolder
+	  , buildMessageAuthor
+	  , buildMessageContent
+	  , buildMessageDate
 	  ;
+
+	moment.locale('es-ES');
+
+	buildMessageHolder = function ( value ) {
+		var $messageHolder;
+
+		$messageHolder = $('<div>')
+		  .addClass( 'message-holder' )
+		  .appendTo( $messagesHolder );
+
+		buildMessageAuthor( value ).appendTo( $messageHolder );
+		buildMessageContent( value ).appendTo( $messageHolder );
+		buildMessageDate( value ).appendTo( $messageHolder );
+	};
+
+	buildMessageAuthor = function ( value ) {
+
+		return $('<div>')
+		  .addClass( 'message-part message-author' )
+		  .text( value.doc.author );
+	};
+
+	buildMessageContent = function ( value ) {
+
+		return $('<div>')
+		  .addClass( 'message-part message-content' )
+		  .text( value.doc.message.content );
+	};
+
+	buildMessageDate = function ( value ) {
+
+		return $('<div>')
+		  .addClass( 'message-part message-date' )
+		  .text( moment( value.doc.message.date ).fromNow() );
+	};
 
 	parseMessages = function ( messages ) {
 
-		$.each( messages, function ( index, value ) {
-
-			$('<div>')
-			  .addClass('message')
-			  .text( value.doc.message.content )
-			  .appendTo( $messageHolder );
-		} );
+		messages.forEach( buildMessageHolder );
 	};
 
-	db.allDocs( { include_docs: true }, function ( err, result ) {
+	db.allDocs( { include_docs: true } )
+	.then( function ( result ) {
 
 		parseMessages( result.rows );
+	} )
+	.catch( function ( error ) {
+
+		console.log( 'Hubo un error', error, error.stack );
 	} );
 } );
